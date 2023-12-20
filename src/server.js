6bd2358a92c4c71d9f8854e1ee055e3760e4fefe
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-20 11:05:54 +0200

    add mysql

--- src/server.js
+++ src/server.js
@@ -1,13 +1,28 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mysql = require('mysql2/promise');
 const app = express();
 
 const PORT = 3000;
+
+// Middleware
+app.use(morgan('dev'));
 
 app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+// GET - /api/posts - grazins visus postus
+app.get('/api/posts', (req, res) => {
+  // prisijungti prie DB
+
+  // atlikti veikma
+  // grazinti duomenis
+  // atsijungti nuo DB
+  res.json('all posts');
+});
+
 app.listen(PORT, () =>
   console.log(`Server runing on http://localhost:${PORT}`)
 );
-// app.listen(PORT);
